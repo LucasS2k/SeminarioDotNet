88fc71fb9a3bc5d5027ef13283b03053960957e4
lucass2k <lucasnahuel2000silva@gmail.com>
2026-04-27 07:13:55 -0300

    practica 5

---
+++ Libros/ConsolaUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using practica5;
+// Caso A:
+Libro libro1 = new Libro();
+// Caso B:
+Libro libro2 = new Libro
+{
+Titulo = "C# Moderno",
+FechaPublicacion = new DateTime(2026, 1, 1),
+Precio = 4500.0,
+ISBN = "978-3-16-148410-0"
+};
+// Caso C:
+Libro libro3 = new Libro("978-3-16-148410-0") {Titulo="C# Moderno"};
+libro3.Precio=4500.0;
+// Caso D:
+libro3.ISBN = "978-3-16-148410-9";
+libro3.FechaPublicacion = DateTime.Now;
+libro3.Titulo=".NET Moderno";
---
+++ Libros/Libro/Libro.cs
@@ -0,0 +1,21 @@
+﻿namespace practica5;
+
+public class Libro
+{
+    private readonly int _isbn;
+    private string _titulo;
+    private double _precio;
+    private DateTime _fechaPublicacion;
+    public required string Titulo { get ;init;}
+
+    public double Precio{get;set;}
+
+    public DateTime FechaPublicacion{get;init;}
+    public Libro(int isbn, string titulo, double precio, DateTime fechaPublicacion)
+    {
+        _isbn = isbn;
+        _titulo = titulo;
+        _precio = precio;
+        _fechaPublicacion = fechaPublicacion;
+    }
+}
